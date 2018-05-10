5e5cccf9b9118017b11d706c89e494a4d9cca30e
sarasaurus <bixler.s.e@gmail.com>
2018-05-09 22:35:40 -0700

    added notes and light scaffolding

--- sarah-carl/src/lib/binary-search.js
+++ sarah-carl/src/lib/binary-search.js
@@ -51,4 +51,44 @@ export default class BinarySearchTree {
     }
     return this._find(rootNode.left, value);
   }
+  _findAndRemove(rootNode, value) {
+    if (!rootNode) {
+      return null;
+    } else if (rootNode.value === value) {
+      if (rootNode.left && rootNode.right) {
+        // Find inorder successor of the node. Copy contents of the inorder successor to the node and delete the inorder successor, ie the node that is inOrder in terms of Value 
+       let temp = // find the inorder successor
+        // Copy the inorder successor's content to this node
+        rootNode.value = temp.value
+        // Delete the inorder successor
+        rootNode.right = deleteNode(rootNode.right, temp.value) // not an actual function
+      } else if (rootNode.left) {
+        //  copy the child to the node 
+        // delete the child
+      }  //  copy the child to the node 
+      // delete the child
+    } else if (rootNode.value < value) {
+      return this._find(rootNode.right, value);
+    }
+    return this._find(rootNode.left, value);
+  }
+
+  remove(value) {
+    if (!this.root) {
+      return null;
+    }
+    return this._find(this.root, value);
+
+    /*
+    GENERAL STEPS
+    1. find (value)
+    2. check to see if node has children:
+        2.a if no children
+          remove node
+        2.b if one child
+            copy the child to the node 
+            delete the child
+        2.c if two children
+              Find inorder successor of the node. Copy contents of the inorder successor to the node and delete the inorder successor. Note that inorder predecessor can also be used. */
+  }
 }
